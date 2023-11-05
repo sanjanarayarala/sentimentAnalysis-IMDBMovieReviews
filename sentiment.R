3f902402def8bada69e58c0c7535b2ab8c22502f
Sanjana Rayarala <iamdheerajjj@gmail.com>
2023-11-04 23:13:51 -0500

    Checking if data is biased or not

--- sentiment.R
+++ sentiment.R
@@ -26,3 +26,28 @@ print(missing_values)
 # 4. Checking for Duplicate Entries
 duplicate_entries <- sum(duplicated(data))
 cat("\nNumber of duplicate entries:", duplicate_entries)
+
+# 5. Checking for Bias
+positive_reviews <- data %>% filter(sentiment == "positive")
+negative_reviews <- data %>% filter(sentiment == "negative")
+
+total_positive_reviews <- nrow(positive_reviews)
+total_negative_reviews <- nrow(negative_reviews)
+
+cat("\nClass Distribution:\n")
+cat("Number of Positive Reviews:", total_positive_reviews, "\n")
+cat("Number of Negative Reviews:", total_negative_reviews, "\n")
+
+# Calculate the percentage of positive and negative reviews
+percentage_positive <- (total_positive_reviews / nrow(data)) * 100
+percentage_negative <- (total_negative_reviews / nrow(data)) * 100
+
+cat("\nPercentage of Positive Reviews:", percentage_positive, "\n")
+cat("Percentage of Negative Reviews:", percentage_negative, "\n")
+
+# Check for bias
+if (percentage_positive != percentage_negative) {
+  cat("\nThe dataset may be biased.\n")
+} else {
+  cat("\nThe dataset appears balanced.\n")
+}
